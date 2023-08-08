ad36da34ba492bd74088cb28e457a193448a9d18
Alexisburgos97 <alexisburgos1997@gmail.com>
2023-08-08 19:42:48 -0300

    servicio guardar categoria

--- src/main/java/com/aburgos/inventory/controller/CategoryRestController.java
+++ src/main/java/com/aburgos/inventory/controller/CategoryRestController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aburgos.inventory.model.Category;
 import com.aburgos.inventory.response.CategoryResponseRest;
 import com.aburgos.inventory.services.ICategoryService;
 
@@ -42,5 +45,17 @@ public class CategoryRestController {
 		return response;
 	}
 	
+	/**
+	 * save category
+	 * @param Category
+	 * @return
+	 */
+	@PostMapping("/categories")
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category){
+	
+		ResponseEntity<CategoryResponseRest> response = categoryService.save(category);
+		
+		return response;
+	}
 	
 }
--- src/main/java/com/aburgos/inventory/services/ICategoryService.java
+++ src/main/java/com/aburgos/inventory/services/ICategoryService.java
@@ -2,6 +2,7 @@ package com.aburgos.inventory.services;
 
 import org.springframework.http.ResponseEntity;
 
+import com.aburgos.inventory.model.Category;
 import com.aburgos.inventory.response.CategoryResponseRest;
 
 public interface ICategoryService {
@@ -9,5 +10,7 @@ public interface ICategoryService {
 	public ResponseEntity<CategoryResponseRest> search();
 	
 	public ResponseEntity<CategoryResponseRest> searchById(Long id);
+	
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 
 }
